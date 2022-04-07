710efd3a60391c016c0af7854fd47b73212f4ab9
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2022-04-07 16:55:43 +0800

    Completed Local MongoDB setup

---
+++ Python/SetupMongoDB.py
@@ -0,0 +1,13 @@
+from msilib import add_data
+import pymongo
+import pandas as pd
+import json
+
+#Requires Mongo Docker container to be setup
+client = pymongo.MongoClient("mongodb://localhost:27017")
+df = pd.read_csv("../Dataset/StrokeDataset.csv")
+df = df.iloc[: , 1:]
+data = df.to_dict(orient="records")
+db = client["csc2008project"]
+db.StrokeDataset.drop()
+db.StrokeDataset.insert_many(data)
